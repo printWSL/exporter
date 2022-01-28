4d1712cf3a9031a0f7054409bd4b8f250a122038
wangshile <1160029860@qq.com>
2022-01-28 13:58:42 +0800

    练习内容

---
+++ exporter_study/node_exporter/node_exporter.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
+	promcollectors "github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/common/promlog"      // 定义了prometheus日志初始化格式
+	"github.com/prometheus/common/promlog/flag" // 命令行参数解析工具
+	"github.com/prometheus/common/version"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+// handler 实现了http包中Handler的ServeHTTP方法
+
+/*
+type Handler interface {
+	ServeHTTP(ResponseWriter, *Request)
+}
+*/
+type handler struct {
+	unfilteredHandler http.Handler
+	// exporterMetricsRegistry is a separate registry for the metrics about
+	// the exporter itself.
+	exporterMetricsRegistry *prometheus.Registry
+	includeExporterMetrics  bool
+	maxRequests             int
+	logger                  log.Logger
+}
+
+func newHandler(includeExporterMetrics bool, maxRequests int, logger log.Logger) *handler {
+	h := &handler{
+		exporterMetricsRegistry: prometheus.NewRegistry(),
+		includeExporterMetrics:  includeExporterMetrics,
+		maxRequests:             maxRequests,
+		logger:                  logger,
+	}
+
+	if h.includeExporterMetrics {
+		h.exporterMetricsRegistry.MustRegister(
+			promcollectors.NewProcessCollector(promcollectors.ProcessCollectorOpts{}),
+			promcollectors.NewGoCollector(),
+		)
+	}
+	if innerHandler, err := h.innerHandler(); err != nil {
+		panic(fmt.Sprintf("Couldn't create metrics handler: %s", err))
+	} else {
+		h.unfilteredHandler = innerHandler
+	}
+
+	return h
+
+}
+
+func main() {
+
+	// 返回指定flag参数，不指定使用默认值
+	var (
+		listenAddress = kingpin.Flag(
+			"web.listen-address",
+			"Address on which to expose metrics and web interface.",
+		).Default(":9100").String()
+		metricsPath = kingpin.Flag(
+			"web.telemetry-path",
+			"Path under which to expose metrics.",
+		).Default("/metrics").String()
+		disableExporterMetrics = kingpin.Flag(
+			"web.disable-exporter-metrics",
+			"Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).",
+		).Bool()
+		maxRequests = kingpin.Flag(
+			"web.max-requests",
+			"Maximum number of parallel scrape requests. Use 0 to disable.",
+		).Default("40").Int()
+		disableDefaultCollectors = kingpin.Flag(
+			"collector.disable-defaults",
+			"Set all collectors to disabled by default.",
+		).Default("false").Bool()
+		configFile = kingpin.Flag(
+			"web.config",
+			"[EXPERIMENTAL] Path to config yaml file that can enable TLS or authentication.",
+		).Default("").String()
+	)
+
+	fmt.Println(*listenAddress, *metricsPath, *disableExporterMetrics, *maxRequests, *disableDefaultCollectors, *configFile)
+
+	promlogConfig := &promlog.Config{}                //提供 --log.level=info
+	flag.AddFlags(kingpin.CommandLine, promlogConfig) //提供 -log.format=logfmt
+
+	kingpin.Version(version.Print("node_exporter"))
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	logger := promlog.New(promlogConfig)
+
+	version.BuildUser = "wshile"
+	version.BuildDate = "2022.03"
+
+	level.Info(logger).Log("msg", "Starting node_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+	// 判断当前如果是root运行就输出warn日志
+	if user, err := user.Current(); err == nil && user.Uid == "0" {
+		level.Warn(logger).Log("msg", "Node Exporter is running as root user. This exporter is designed to run as unpriviledged user, root is not required.")
+	}
+
+	http.Handle(*metricsPath, newHandler(!*disableExporterMetrics, *maxRequests, logger))
+}
